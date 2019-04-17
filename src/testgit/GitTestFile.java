9a7178e94203942076a05ad731a019dfce8274f9
sunzd2017 <33246561+sunzd2017@users.noreply.github.com>
2019-04-17 23:33:39 +0800

    Update GitTestFile.java

--- src/testgit/GitTestFile.java
+++ src/testgit/GitTestFile.java
@@ -4,9 +4,11 @@ public class GitTestFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 0;
 		System.out.println();
 		System.out.println(124);
-		
+		int a = 5;
+		System.out.println(14);
 	}
 
 }
