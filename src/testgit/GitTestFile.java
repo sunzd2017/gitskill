bb5f1150c3ecc6c77870a3e50cb9ce47f4fd56af
sunzd <sunzd2010@126.com>
2019-04-17 23:35:23 +0800

    test

--- src/testgit/GitTestFile.java
+++ src/testgit/GitTestFile.java
@@ -4,9 +4,10 @@ public class GitTestFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int b = 4;
 		System.out.println();
 		System.out.println(124);
-		
+		String str = "";
 	}
 
 }
